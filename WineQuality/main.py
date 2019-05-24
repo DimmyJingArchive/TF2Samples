2a0fd5198533e1b8cd489cdb3a5fe926a30bddf0
PotatosFish <jimmyguding@gmail.com>
2019-05-24 08:59:49 -0500

    modified stuff

--- WineQuality/main.py
+++ WineQuality/main.py
@@ -1,4 +1,6 @@
+from tensorflow.data.Dataset import from_tensor_slices
 from tensorflow.python import keras
+import tensorflow as tf
 import pandas as pd
 import numpy as np
 
@@ -15,27 +17,48 @@ train_y = train_data.pop('quality').values
 mins, maxs = train_data.values.min(0), train_data.values.max(0)
 # Normalize the data
 train_x = (train_data.values - mins) / maxs
+train_ds = from_tensor_slices((train_x, train_y)).shuffle(10000).batch(32)
 
 # Drops training data from raw data to get test data
 test_data = data.drop(train_data.index)
 # Same as above
 test_y = test_data.pop('quality').values
 test_x = (test_data.values - mins) / maxs
+test_ds = from_tensor_slices((test_x, test_y)).shuffle(10000).batch(32)
+
 
 # Creates a sequential networks with three layers
 # First layer and second layer contain 100 neurons,
 # and have activation function of RELU.
 # Last layer yields the result through linear activation
-model = keras.Sequential([
-    keras.layers.Dense(100, activation='relu'),
-    keras.layers.Dense(100, activation='relu'),
-    keras.layers.Dense(1, activation='linear')
-])
-# Compiles the model with adam optimizer, mean square error loss,
-# and root mean squared error metric
-model.compile(optimizer='adam', loss='mse',
-              metrics=[keras.metrics.RootMeanSquaredError(name='rmse')])
-# Fit the model with train data for 100 epochs, might overfit if run more
-model.fit(train_x, train_y, epochs=100)
-# Evaluate the model using test dataset
-model.evaluate(test_x, test_y)
+class Model(keras.Model):
+    def __init__(self):
+        super().__init__()
+        self.d1 = keras.layers.Dense(100, activation='relu')
+        self.d2 = keras.layers.Dense(100, activation='relu')
+        self.res = keras.layers.Dense(1, activation='linear')
+
+    def call(self, x):
+        x = self.d1(x)
+        x = self.d2(x)
+        return self.res(x)
+
+
+model = Model()
+loss_object = keras.losses.MeanSquaredError(name='mse')
+optimizer = keras.optimizers.Adam(name='adam')
+metric = keras.metrics.RootMeanSquaredError(name='rmse')
+
+
+@tf.function
+def train_step(features, labels):
+    with tf.GradientTape() as tape:
+        predictions = model(features)
+        loss = loss_object(labels, predictions)
+        gradients = tape.gradient(loss, model.trainable_variables)
+        optimizer.apply_gradients(zip(gradients, model.trainable_variables))
+    metric(loss)
+
+
+for features, labels in train_ds:
+    train_step(features, labels)
